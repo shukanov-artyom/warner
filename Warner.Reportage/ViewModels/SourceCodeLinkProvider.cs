e0f3accffcfc604de85f1059c299c24b409d4bea
Artyom Shukanov <shukanov.artyom@gmail.com>
2017-09-04 16:57:29 +0300

    Fix minor issue

---
+++ Warner.Reportage/ViewModels/SourceCodeLinkProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Warner.Reportage.ViewModels
+{
+    public class SourceCodeLinkProvider
+    {
+        private readonly string sourceFile;
+
+        public SourceCodeLinkProvider(string sourceFile)
+        {
+            this.sourceFile = sourceFile;
+        }
+
+        public string Provide()
+        {
+            return String.Format(
+                "https://lineage.beanstalkapp.com/prod-orthobullets-com/browse/dev-branches/merge-websites/{0}",
+                sourceFile);
+        }
+    }
+}
