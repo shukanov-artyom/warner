2f3e6cd7e60a9cc00da2166c2c68a593021b3d3b
Artyom Shukanov <shukanov.artyom@gmail.com>
2017-09-13 21:56:53 +0300

    LESS infrastructure altered

--- Warner.Reportage/gulpfile.js
+++ Warner.Reportage/gulpfile.js
@@ -1,9 +1,9 @@
-﻿var gulp = require('gulp'),
-    fs = require('fs'),
-    less = require('gulp-less');
+﻿var gulp = require("gulp"),
+    fs = require("fs"),
+    less = require("gulp-less");
 
-gulp.task('default', function () {
-    return gulp.src('Styles/main.less')
-        .pipe('less')
-        .pipe(gulp.dest('wwwroot/css'));
+gulp.task("default", function () {
+    return gulp.src("Styles/main.less")
+        .pipe(less())
+        .pipe(gulp.dest("wwwroot/css"));
 });
